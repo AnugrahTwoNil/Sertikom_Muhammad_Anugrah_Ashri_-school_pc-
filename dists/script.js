76a9d922f76a9f12f4284e72cf8610893d2bfa92
AnugrahTwoNil <grahamanug@Gmail.com>
2025-12-02 09:18:38 +0700

    first commit

---
+++ dists/script.js
@@ -0,0 +1,38 @@
+const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+const form = document.getElementById("MyForm");
+const NamaInput = document.getElementById('nama');
+const EmailInput = document.getElementById('email');
+const PesanInput = document.getElementById('pesan');
+const NamaError = document.getElementById('namaError');
+const EmailError = document.getElementById('emailError');
+const PesanError = document.getElementById('pesanError');
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault()
+
+    NamaError, EmailError, PesanError.innerText = "";
+
+    if (NamaInput.value.trim() === "") {
+        NamaError.innerText = "Nama Tidak Boleh Kosong.";
+    }
+
+    if (EmailInput.value.trim() === "") {
+        EmailError.innerText = "Email Tidak Boleh Kosong.";
+    } else if (!emailPattern.test(EmailInput.value.trim())) {
+        EmailError.innerText = "Format Email Salah.";
+    }
+
+    if (PesanInput.value.trim() === "") {
+        PesanError.innerText = "Pesan Tidak Boleh Kosong.";
+    }
+});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 5,
+    breakpoints: {
+        768: { slidesPerView: 2 },
+        1024: { slidesPerView: 4 }
+    },
+});
+
